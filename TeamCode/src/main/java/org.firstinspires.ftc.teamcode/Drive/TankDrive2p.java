4c4abad43e64a58f8c6a1a1836660055dcb5af29
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2023-10-14 21:18:04 -0600

    Rename variables to "type"-"class"
    eg. motorLeft becomes leftMotor

--- TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Drive/TankDrive2p.java
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Drive/TankDrive2p.java
@@ -9,31 +9,31 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class TankDrive2p extends OpMode {
     /* Initialization */
     // Drivetrain init
-    protected DcMotor motorLeft;
-    protected DcMotor motorRight;
+    protected DcMotor leftMotor;
+    protected DcMotor rightMotor;
     // Claw init
-    protected Servo clawLeft;
-    protected Servo clawRight;
+    protected Servo leftClaw;
+    protected Servo rightClaw;
     // Slide init
-    protected DcMotor motorSlide;
+    protected DcMotor slideMotor;
 
     @Override
     public void init() {
         // hardwareMap drivetrain motors
-        motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
-        motorRight = hardwareMap.get(DcMotor.class, "motorRight");
+        leftMotor = hardwareMap.get(DcMotor.class, "motorLeft");
+        rightMotor = hardwareMap.get(DcMotor.class, "motorRight");
         // Set drivetrain RunModes
-        motorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // hardwareMap claw servos
-        clawLeft = hardwareMap.get(Servo.class, "clawLeft");
-        clawRight = hardwareMap.get(Servo.class, "clawRight");
+        leftClaw = hardwareMap.get(Servo.class, "clawLeft");
+        rightClaw = hardwareMap.get(Servo.class, "clawRight");
 
         // hardwareMap slide motor
-        motorSlide = hardwareMap.get(DcMotor.class, "motorSlide");
+        slideMotor = hardwareMap.get(DcMotor.class, "motorSlide");
         // Set slide RunMode
-        motorSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        slideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Telemetry
         telemetry.addData("STATUS", "Initialized"); // the FTC equivalent to println()
@@ -43,21 +43,21 @@ public class TankDrive2p extends OpMode {
     @Override
     public void loop() {
         // Update drive system
-        motorLeft.setPower(gamepad1.left_stick_y);
-        motorRight.setPower(gamepad1.right_stick_y);
+        leftMotor.setPower(gamepad1.left_stick_y);
+        rightMotor.setPower(gamepad1.right_stick_y);
 
         // Update claw
         if (gamepad2.a) {
-            clawLeft.setPosition(0.595);
-            clawRight.setPosition(0.73);
+            leftClaw.setPosition(0.595);
+            rightClaw.setPosition(0.73);
         }
         if (gamepad2.b) {
-            clawLeft.setPosition(0.70);
-            clawRight.setPosition(0.61);
+            leftClaw.setPosition(0.70);
+            rightClaw.setPosition(0.61);
         }
 
         // Update slide
-        motorSlide.setPower(gamepad2.right_stick_y);
+        slideMotor.setPower(gamepad2.right_stick_y);
 
         // Telemetry
         telemetry(telemetry);
@@ -65,17 +65,17 @@ public class TankDrive2p extends OpMode {
 
     public void telemetry(Telemetry telemetry) {
         telemetry.addData("\nCurrent section", "Drivetrain");
-        telemetry.addData("runMode", motorLeft.getMode());
+        telemetry.addData("runMode", leftMotor.getMode());
         telemetry.addData("Left Power",
-                "%4.2f", motorLeft.getPower());
+                "%4.2f", leftMotor.getPower());
         telemetry.addData("Right Power",
-                "%4.2f", motorRight.getPower());
+                "%4.2f", rightMotor.getPower());
 
         telemetry.addData("\nCurrent section", "Claw");
-        telemetry.addData("Left claw position", clawLeft.getPosition());
-        telemetry.addData("Right claw position", clawRight.getPosition());
+        telemetry.addData("Left claw position", leftClaw.getPosition());
+        telemetry.addData("Right claw position", rightClaw.getPosition());
 
         telemetry.addData("\nCurrent section", "Slide");
-        telemetry.addData("Slide Y", motorSlide.getCurrentPosition());
+        telemetry.addData("Slide Y", slideMotor.getCurrentPosition());
     }
 }
